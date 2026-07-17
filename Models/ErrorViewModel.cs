248f5b905d977a491018e4e22996975eef8bcde5
OrtegaSanz <carlos-eduardo-1995@live.com>
2026-07-17 11:32:47 -0500

    Inicialización del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Registro_de_personas_prueba.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
